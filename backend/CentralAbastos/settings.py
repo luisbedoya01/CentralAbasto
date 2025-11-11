995133b195d16baed97a5409d358aa295f073fbf
Ivan Castro Abad <luis.bedoyaj@ug.edu.ec>
2025-11-11 12:06:20 -0500

    Cambio en el debug

--- backend/CentralAbastos/settings.py
+++ backend/CentralAbastos/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)igdp4_$7=n3^ijf0offh-uaq#ux&akzos2-ifzjmo$q2jax0)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 # Modificacion
 ALLOWED_HOSTS = [
